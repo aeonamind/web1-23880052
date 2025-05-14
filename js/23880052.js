65f6631adc429cc2d1616a4c4fc25bb06a4a5d42
Trung Nguyen Nguyen <aeonamind@gmail.com>
2025-05-15 02:08:28 +0700

    feat: blogs, contacts pages

--- js/23880052.js
+++ js/23880052.js
@@ -1,12 +1,29 @@
-const API = "https://web1-api.vercel.app/api"
+const API = 'https://web1-api.vercel.app/api';
+const AUTHENTICATE_API = 'https://web1-api.vercel.app/users';
 
 async function loadData(request, templateId, viewId) {
-    const response = await fetch(`${API}/${request}`);
-    const data = await response.json();
+  const response = await fetch(`${API}/${request}`);
+  const data = await response.json();
 
-    var source = document.getElementById(templateId).innerHTML;
-    var template = Handlebars.compile(source);
-    var context = { data: data };
-    var view = document.getElementById(viewId);
-    view.innerHTML = template(context);
+  var source = document.getElementById(templateId).innerHTML;
+  var template = Handlebars.compile(source);
+  var context = { data: data };
+  var view = document.getElementById(viewId);
+  view.innerHTML = template(context);
+}
+
+async function getAuthenticateToken(username, password) {
+  let response = await fetch(`${AUTHENTICATE_API}/authenticate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+    body: JSON.stringify({ username, password }),
+  });
+  let result = response.json();
+  if (response.status == 200) {
+    return result.token;
+  }
+  throw new Error(result.message);
 }
