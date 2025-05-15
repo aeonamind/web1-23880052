fba12ef1558cb20ebc9e9579d760e32c3235aed1
Trung Nguyen Nguyen <aeonamind@gmail.com>
2025-05-16 00:53:44 +0700

    feat: login, logout, add comments, search

--- js/23880052.js
+++ js/23880052.js
@@ -21,9 +21,80 @@ async function getAuthenticateToken(username, password) {
     },
     body: JSON.stringify({ username, password }),
   });
-  let result = response.json();
+  let result = await response.json();
   if (response.status == 200) {
     return result.token;
   }
   throw new Error(result.message);
+}
+
+// username: web1
+// password: W3b1@Project
+async function login(e) {
+  e.preventDefault();
+
+  let username = document.getElementById('username').value;
+  let password = document.getElementById('password').value;
+  document.getElementById('errorMessage').innerHTML = '';
+
+  try {
+    let token = await getAuthenticateToken(username, password);
+    if (token) {
+      localStorage.setItem('token', token);
+      document.getElementsByClassName('btn-close')[0].click();
+      displayControls();
+    }
+  } catch (error) {
+    document.getElementById('errorMessage').innerHTML = error;
+    displayControls(false);
+  }
+}
+
+function displayControls(isLogin = true) {
+  let linkLogins = document.getElementsByClassName('linkLogin');
+  let linkLogouts = document.getElementsByClassName('linkLogout');
+
+  let displayLogin = 'none';
+  let displayLogout = 'block';
+  if (!isLogin) {
+    displayLogin = 'block';
+    displayLogout = 'none';
+  }
+
+  for (let i = 0; i < 2; i++) {
+    linkLogins[i].style.display = displayLogin;
+    linkLogouts[i].style.display = displayLogout;
+  }
+
+  let leaveComment = document.getElementById('leave-comment');
+  if (leaveComment) leaveComment.style.display = displayLogout;
+}
+
+async function checkLogin() {
+  let isLogin = await verifyToken();
+  displayControls(isLogin);
+}
+
+async function verifyToken() {
+  let token = localStorage.getItem('token');
+  if (token) {
+    let response = await fetch(`${AUTHENTICATE_API}/verify`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    });
+
+    if (response.status == 200) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function logout() {
+  localStorage.clear();
+  displayControls(false);
 }
